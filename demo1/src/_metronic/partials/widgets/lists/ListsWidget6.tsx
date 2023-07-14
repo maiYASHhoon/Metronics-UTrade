328feae1e99188d2a20e8a109bc64e3d6112d511
maiYASHhoon <yash280801@gmail.com>
2023-07-14 15:21:03 +0530

    add-customer design

--- demo1/src/_metronic/partials/widgets/lists/ListsWidget6.tsx
+++ demo1/src/_metronic/partials/widgets/lists/ListsWidget6.tsx
@@ -7,7 +7,7 @@ const ListsWidget6 = () => {
   return (
     <div className='row justify-content-between'>
       <div className='col-md-3 col-sm-6'>
-        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#d4e1fc'}}>
+        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#d4e1fc', boxShadow: 'none'}}>
           <div className='card-body'>
             <div className='flex-grow-1 me-2'>
               <a className='fw-bolder text-gray-800 text-hover-primary fs-22'>TSH 462,128</a>
@@ -17,7 +17,7 @@ const ListsWidget6 = () => {
         </div>
       </div>
       <div className='col-md-3 col-sm-6 card'>
-        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#ccebfd'}}>
+        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#ccebfd', boxShadow: 'none'}}>
           <div className='card-body'>
             <div className='flex-grow-1 me-2'>
               <a className='fw-bolder text-gray-800 text-hover-primary fs-22'>1200</a>
@@ -28,7 +28,7 @@ const ListsWidget6 = () => {
       </div>
 
       <div className='col-md-3 col-sm-6 card'>
-        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#ccf3f0'}}>
+        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#ccf3f0', boxShadow: 'none'}}>
           <div className='card-body'>
             <div className='flex-grow-1 me-2'>
               <a className='fw-bolder text-gray-800 text-hover-primary fs-22'>35</a>
@@ -38,7 +38,7 @@ const ListsWidget6 = () => {
         </div>
       </div>
       <div className='col-md-3 col-sm-6 card'>
-        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#fbebbc'}}>
+        <div className='card rounded-4 mb-5 mb-md-0' style={{backgroundColor: '#fbebbc', boxShadow: 'none'}}>
           <div className='card-body'>
             <div className='flex-grow-1 me-2'>
               <a className='fw-bolder text-gray-800 text-hover-primary fs-22'>Tsh 13,757,564</a>
--- demo1/src/app/pages/add-customer/addCustomer.tsx
+++ demo1/src/app/pages/add-customer/addCustomer.tsx
@@ -1,8 +1,30 @@
-import React, {FC} from 'react'
+import React, {FC, useEffect, useState} from 'react'
 import {KTSVG, toAbsoluteUrl} from '../../../_metronic/helpers'
 import Select from 'react-select'
+import clsx from 'clsx'
+
+const optionsYear = [
+  {value: 'Business 1', label: 'Business Really'},
+  {value: 'Business 2', label: 'Business Really'},
+  {value: 'Business 3', label: 'Business Really'},
+]
+const customStyles = {
+  control: (provided: any) => ({
+    ...provided,
+    width: 'auto',
+    height: '43px',
+    borderRadius: '8px',
+    border: 'solid 0.5px #e0e0df',
+    backgroundColor: 'white',
+    fontSize: '15px',
+  }),
+}
+
+const defaultOption2 = {value: 'Select business type', label: 'Select business type'}
 
 const AddCustomer: FC = () => {
+  const [loading, setLoading] = useState(false)
+
   return (
     <>
       <div className='row align-items-center mb-5 pt-5'>
@@ -10,11 +32,14 @@ const AddCustomer: FC = () => {
           <h1 className='fs-22 text-bold'>Add new customer</h1>
         </div>
       </div>
+
+      {/* {*****************************************************************************************} */}
+      {/* { begin } */}
       <div className='row g-9'>
         <div className='col-lg-4 col-xxl-3'>
-          <div className='card bg-light rounded-2 '>
+          <div className='card bg-light rounded-2 ' style={{boxShadow: 'none'}}>
             <div className='card-body '>
-              <div className='me-2 text-left'>
+              <div className='me-2 text-left mb-2'>
                 <span className='fw-bolder text-gray-800 fs-22'>Business Logo</span>
               </div>
               <div className='pt-1 d-flex justify-content-center align-items-center'>
@@ -27,23 +52,252 @@ const AddCustomer: FC = () => {
             </div>
           </div>
         </div>
+        {/* {*****************************************************************************************} */}
         <div className='col-lg-8 col-xxl-9 '>
-          <div className='card bg-light rounded-2 '>
-            <div className='card-body '>
-              <div className=' me-2'>
-                <span className='fw-bolder text-gray-800 fs-22'>Business Logo</span>
-                <div>
-                  <img
-                    className='upload-logo'
-                    src={toAbsoluteUrl('/media/customers/upload.png')}
-                    alt=''
-                  />
+          <div className='row'>
+            <div className='col-12'>
+              <div className='card bg-light rounded-2 ' style={{boxShadow: 'none'}}>
+                <div className='card-body '>
+                  <div className=' me-2 mb-3'>
+                    <span className='fw-bolder text-gray-800 fs-22'>Customer details</span>
+                  </div>
+                  <div className='row'>
+                    <div className='col-lg-6 '>
+                      <div className='mb-2 pt-2 fs-16 fw-bolder'>
+                        <span>Name</span>
+                      </div>
+                      <div className='fv-row mb-2'>
+                        <input
+                          placeholder='Type here....'
+                          className={clsx('form-control bg-white')}
+                          type='email'
+                          name='email'
+                          autoComplete='off'
+                        />
+                      </div>
+                    </div>
+                    <div className='col-lg-6'>
+                      <div className='mb-2 pt-2 fw-bolder fs-16'>
+                        <span>Email</span>
+                      </div>
+                      <div className='fv-row mb-2'>
+                        <input
+                          placeholder='Type here...'
+                          className={clsx('form-control bg-white')}
+                          type='email'
+                          name='email'
+                          autoComplete='off'
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col-lg-6 pt-5'>
+                      <div className='mb-2 fs-16 fw-bold'>
+                        <span>Phone number</span>
+                      </div>
+
+                      <div className='input-group'>
+                        <div className='border-right-0 bg-white fs-16 fw-600 text-black px-6 form is-invalid input-group-text'>
+                          +91
+                        </div>
+                        <div className='d-flex flex-grow-1'>
+                          <input
+                            placeholder='phone number...'
+                            type='string'
+                            autoComplete='off'
+                            className={clsx('form-control bg-white ')}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+              </div>
+            </div>
+            {/* {*****************************************************************************************} */}
+            <div className='col-12 mt-7'>
+              <div className='card bg-light rounded-2 ' style={{boxShadow: 'none'}}>
+                <div className='card-body '>
+                  <div className=' me-2 mb-3'>
+                    <span className='fw-bolder text-gray-800 fs-22'>Business details</span>
+                  </div>
+                  <div className='row'>
+                    <div className='col-lg-6 '>
+                      <div className='mb-2 pt-2 fs-16 fw-bolder'>
+                        <span>Name</span>
+                      </div>
+                      <div className='fv-row mb-2'>
+                        <input
+                          placeholder='Type here....'
+                          className={clsx('form-control bg-white')}
+                          type='email'
+                          name='email'
+                          autoComplete='off'
+                        />
+                      </div>
+                    </div>
+                    <div className='col-lg-6'>
+                      <div className='mb-2 pt-2 fw-bolder fs-16'>
+                        <span>Email</span>
+                      </div>
+                      <div className='fv-row mb-2'>
+                        <Select
+                          options={optionsYear}
+                          styles={customStyles}
+                          defaultValue={defaultOption2}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col-lg-12 pt-5'>
+                      <div className='mb-2 fs-16 fw-bolder'>
+                        <span>Address</span>
+                      </div>
+                      <div className='fv-row mb-2'>
+                        <input
+                          placeholder='Type here....'
+                          className={clsx('form-control bg-white')}
+                          type='email'
+                          name='email'
+                          autoComplete='off'
+                        />
+                      </div>
+
+                      <div className='row'>
+                        <div className='col-lg-6 '>
+                          <div className='mb-2 pt-2 fs-16 fw-bolder'>
+                            <span>TIN Number</span>
+                          </div>
+                          <div className='fv-row mb-2'>
+                            <input
+                              placeholder='Type here....'
+                              className={clsx('form-control bg-white')}
+                              type='email'
+                              name='email'
+                              autoComplete='off'
+                            />
+                          </div>
+                        </div>
+                        <div className='col-lg-6'>
+                          <div className='mb-2 pt-2 fw-bolder fs-16'>
+                            <span>VAT Number</span>
+                          </div>
+                          <div className='fv-row mb-2'>
+                            <input
+                              placeholder='Type here...'
+                              className={clsx('form-control bg-white')}
+                              type='email'
+                              name='email'
+                              autoComplete='off'
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-lg-12 text-end'>
+                <button className='btn btn-primary mb-1 mt-8'>Add Customer</button>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {/* {end } */}
+
+      {/* {Row 2 : begin } */}
+      {/* <div className='row'>
+        <div className='col-lg-8 col-xxl-9 pt-12'>
+          <div className='card bg-light rounded-2 ' style={{boxShadow: 'none'}}>
+            <div className='card-body '>
+              <div className=' me-2 mb-3'>
+                <span className='fw-bolder text-gray-800 fs-22'>Business details</span>
+              </div>
+              <div className='row'>
+                <div className='col-lg-6 '>
+                  <div className='mb-2 pt-2 fs-16 fw-bolder'>
+                    <span>Name</span>
+                  </div>
+                  <div className='fv-row mb-2'>
+                    <input
+                      placeholder='Type here....'
+                      className={clsx('form-control bg-white')}
+                      type='email'
+                      name='email'
+                      autoComplete='off'
+                    />
+                  </div>
+                </div>
+                <div className='col-lg-6'>
+                  <div className='mb-2 pt-2 fw-bolder fs-16'>
+                    <span>Email</span>
+                  </div>
+                  <div className='fv-row mb-2'>
+                    <Select
+                      options={optionsYear}
+                      styles={customStyles}
+                      defaultValue={defaultOption2}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-lg-12 pt-5'>
+                  <div className='mb-2 fs-16 fw-bolder'>
+                    <span>Address</span>
+                  </div>
+                  <div className='fv-row mb-2'>
+                    <input
+                      placeholder='Type here....'
+                      className={clsx('form-control bg-white')}
+                      type='email'
+                      name='email'
+                      autoComplete='off'
+                    />
+                  </div>
+
+                  <div className='row'>
+                    <div className='col-lg-6 '>
+                      <div className='mb-2 pt-2 fs-16 fw-bolder'>
+                        <span>TIN Number</span>
+                      </div>
+                      <div className='fv-row mb-2'>
+                        <input
+                          placeholder='Type here....'
+                          className={clsx('form-control bg-white')}
+                          type='email'
+                          name='email'
+                          autoComplete='off'
+                        />
+                      </div>
+                    </div>
+                    <div className='col-lg-6'>
+                      <div className='mb-2 pt-2 fw-bolder fs-16'>
+                        <span>VAT Number</span>
+                      </div>
+                      <div className='fv-row mb-2'>
+                        <input
+                          placeholder='Type here...'
+                          className={clsx('form-control bg-white')}
+                          type='email'
+                          name='email'
+                          autoComplete='off'
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      {/* {Row 2 : end } */}
     </>
   )
 }
